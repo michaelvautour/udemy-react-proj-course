d3ddb59264b546b9c4193760e77753fbab6a2135
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-03 18:42:11 -0600

    Example of passing props through components

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,23 @@ import Person from "./Components/Person";
 
 // CSS
 import "./index.css";
+// setup vars
+// const author = "George R. R. Martin";
+// const title = "A Song of Ice & Fire";
+// const img =
+//   "https://m.media-amazon.com/images/I/91GcaFy+4GL._AC_UL640_FMwebp_QL65_.jpg";
+
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/51rW2153DLL._SL500_.jpg",
+  title: "I love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/91GcaFy+4GL._AC_UL640_FMwebp_QL65_.jpg",
+  title: "A Song of Ice & Fire",
+  author: "George R. R. Martin",
+};
 
 const BookList = () => {
   return (
@@ -14,49 +31,29 @@ const BookList = () => {
       <Message />
       This is a booklist
       <br />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 };
 
-const author = "George R. R. Martin";
-
-const Book = () => {
-  const title = "A Song of Ice & Fire";
-
+const Book = (props: any) => {
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/91GcaFy+4GL._AC_UL640_FMwebp_QL65_.jpg"
-        alt="Game of Thrones Boxset"
-        width="50%"
-        height="50%"
-      />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* Cannot add JS directly in JSX that does not return a value, always need to pass
-      the value (i.e. cannot be a statement)
-      <p>{let x = 6}</p> 
-      As shown in below it'll return as it returns a value:
-      */}
-      <p>{6 + 6}</p>
+      <img src={props.img} alt="" width="50%" height="50%" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
-
-// example of css in direct JSX
-// First {} after style is jumping in JavaScript
-// Second one is an object that's required
-// My explanation likely needs some refining
-// properties are written normal, but values need to be in ""
-// inline css directly in the item will override (similar to non-react)
-// example no longer used below:
-
-/* <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-  George R. R. Martin
-</h4>; */
-
-// ReactDOM.render(<BookList />, document.getElementById("root"));
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
