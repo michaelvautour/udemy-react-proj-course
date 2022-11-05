37678929c8c0144436a4e17e5a8eb906cab3f55a
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-05 10:11:09 -0600

    Array destructuring,  and rendered onto page

--- src/index.tsx
+++ src/index.tsx
@@ -7,59 +7,56 @@ import Person from "./Components/Person";
 // CSS
 import "./index.css";
 // setup vars
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/51rW2153DLL._SL500_.jpg",
-  title: "I love You to the Moon and Back",
-  author: "Amelia Hepworth",
-};
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/51rW2153DLL._SL500_.jpg",
+    title: "I love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/91GcaFy+4GL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "A Song of Ice & Fire",
+    author: "George R. R. Martin",
+  },
+];
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/91GcaFy+4GL._AC_UL640_FMwebp_QL65_.jpg",
-  title: "A Song of Ice & Fire",
-  author: "George R. R. Martin",
-};
+const names = ["john", "peter", "susan"];
+
+const newNames = names.map((name) => {
+  console.log("name Arr", name);
+  return <h1>{name}</h1>;
+});
+
+console.log("newNames Arr", newNames);
 
 type Props = {
   img: string;
   title: string;
   author: string;
-  children?: JSX.Element;
+  children?: React.ReactNode;
 };
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Person />
-      <Message />
-      This is a booklist
-      <br />
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iusto
-          sapiente laboriosam beatae, fugiat quod est illo dolore asperiores
-          odio?
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      <>
+        {newNames}
+        <Person />
+        <Message />
+        This is a booklist
+        <br />
+        <Book img={""} title={""} author={""} />
+      </>
     </section>
   );
 };
 
-const Book = ({ img, title, author, children }: Props) => {
+const Book = ({ img, title, author }: Props) => {
   return (
     <article className="book">
       <img src={img} alt="" width="50%" height="50%" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
